8499405ecceb95ab735659810a37d7417c3571ea
alex <alexcarvajal2404@gmail.com>
2021-11-07 10:51:22 -0400

    initial requests with express from server file

--- server.js
+++ server.js
@@ -6,7 +6,37 @@ dotenv.config({path: './config/config.env'});
 
 const app = express();
 
-const PORT = process.env.PORT;
+app.get('/api/v1/bootcamps', (req, res) => {
+    res
+      .status(200)
+      .json({ success: true, msg: 'Show all the bootcamps' });
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res
+      .status(200)
+      .json({ success: true, msg: `Show all single bootcamp ${req.params.id}`});
+})
+
+app.post('/api/v1/bootcamps/', (req, res) => {
+    res
+      .status(200)
+      .json({ success: true, msg: `Create a bootcamp`});
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res
+      .status(200)
+      .json({ success: true, msg: `update a bootcamp ${req.params.id}`});
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res
+      .status(200)
+      .json({ success: true, msg: `delete a bootcamp ${req.params.id}`});
+})
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
